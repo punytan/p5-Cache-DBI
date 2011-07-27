e44e1f8f0b4cb34446e0cd3e78bc64028c029e0d
punytan <punytan@gmail.com>
2011-07-27 10:48:38 +0900

    initial commit

---
+++ lib/Cache/DBI.pm
@@ -0,0 +1,32 @@
+package Cache::DBI;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Cache::DBI -
+
+=head1 SYNOPSIS
+
+  use Cache::DBI;
+
+=head1 DESCRIPTION
+
+Cache::DBI is
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
